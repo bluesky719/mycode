48d430eb0f59226e43e445733dd8f2a45ba1f294
bluesky719 <blusky719@gmail.com>
2014-08-07 10:42:45 +0800

    initial version

---
+++ UI/Button/Hamburger/HamburgerButton/HamburgerButton/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HamburgerButton
+//
+//  Created by yf6190 on 8/7/14.
+//  Copyright (c) 2014 yfstudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UI/Button/Hamburger/HamburgerButton/HamburgerButton/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HamburgerButton
+//
+//  Created by yf6190 on 8/7/14.
+//  Copyright (c) 2014 yfstudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
